250425ca0e634d733ebb65fae8fa9bcfcd069684
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-14 17:31:34 +0300

    solution this.vakue

--- src/debounce.js
+++ src/debounce.js
@@ -1,36 +1,38 @@
 'use strict';
 
-/** 
- * Реализовать декоратор debounce
- * 
- * Чтобы оригинальная функция запускалась только после определённой паузы в запусках обёртки
- * Пока пользователь печататет - ничего
+/**
+ * Implement decorator debounce
+ *
+ * Чтобы оригинальная функция запускалась только после определённой паузы
+ * в запусках обёртки, пока пользователь печататет - ничего
  * Если остановился и подождал секунду - вывести последнее значение
  * Функция onChange должна получать тот же `this` и аргументы, что и обёртка
  **/
 
-function debounce(f, delay) {
+function debounce(func, delay) {
   let timer;
-  
-  return (...args) => {
+
+  return function(caller) {
     clearTimeout(timer);
-  
-    timer = setTimeout(function() {
-      f(...args);
-    }, delay);
+
+    const funcCall = () => {
+      return func.call(this, caller);
+    }
+
+    timer = setTimeout(funcCall, delay);
   };
 }
 
 function onChange(event) {
+  output.innerHTML = event.target.value;
   // eslint-disable-next-line no-console
-  console.log(event.target.value);
-  // eslint-disable-next-line no-console
-  // console.log(this.value);
-  // uncomment if you implemented debounce with correct this
+  console.log(this.value);
 }
 
 const wrapper = debounce(onChange, 1000);
 
 const input1 = document.getElementById('input1');
+
+const output = document.querySelector('.output');
 
 input1.addEventListener('input', wrapper);
